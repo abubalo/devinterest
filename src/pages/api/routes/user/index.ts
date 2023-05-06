8b2ecc23f2f6e87aa87c51eeb246747c85497c34
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-06 20:47:11 +0200

    Created routes

---
+++ src/pages/api/routes/user/index.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import UserController from "../../controllers/userController";
+
+const userController = new UserController();
+
+// Get all users
+export default async function GET(req: NextApiRequest, res: NextApiResponse){
+    await userController.getUserById(req, res);
+}
