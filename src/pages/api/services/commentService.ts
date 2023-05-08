0678a5306241b1b437bb54689ae3a16f3038f7ad
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-08 13:40:41 +0200

    implemant create in comment service

--- src/pages/api/services/commentService.ts
+++ src/pages/api/services/commentService.ts
@@ -4,6 +4,7 @@ import { Comment } from "../models/Comment";
 const prisma = new PrismaClient();
 
 export class CommentService{
+
     public async createComment(content: string, author: number, post: number): Promise<Comment>{
         const comment = await prisma.comment.create({
             content,
@@ -13,11 +14,11 @@ export class CommentService{
         return new Comment(comment.id, comment.content, comment.author, comment.post, comment.createdAt);
     }
 
-    public async getComments(): Promise<Comment | null>{
-        const comment = new prisma.comment.findMany();
+    // public async getComments(): Promise<Comment | null>{
+    //     const comment = new prisma.comment.findMany();
 
-        return comment
-    }
+    //     return comment
+    // }
 
 
 }
