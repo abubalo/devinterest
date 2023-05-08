ce686359815e7f57e67ec52343a5c560229c95f2
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-08 13:32:23 +0200

    update change message id to string

--- src/pages/api/models/Message.ts
+++ src/pages/api/models/Message.ts
@@ -1,11 +1,11 @@
 export class Message{
-    private readonly id: number;
+    private readonly id: string;
     private content: string;
     private createdAt: Date;
     private author: number;
     private chat: number;
 
-    constructor(id: number, content: string, createdAt: Date, author: number, chat: number){
+    constructor(id: string, content: string, createdAt: Date, author: number, chat: number){
         this.id = id;
         this.content = content;
         this.createdAt = createdAt;
