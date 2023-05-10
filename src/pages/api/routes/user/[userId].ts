2587e8a1586b0ade9887e4ef7389d97c43fffa1d
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-10 11:03:15 +0200

    feat: implement dynamic request route for user

--- src/pages/api/routes/user/[userId].ts
+++ src/pages/api/routes/user/[userId].ts
@@ -3,6 +3,17 @@ import UserController from "../../controllers/userController";
 
 const userController = new UserController();
 
-export default async function login(req: NextApiRequest, res: NextApiResponse){
-    await userController.login(req, res);
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    switch (req.method) {
+        case "GET":
+            await userController.getUserById(req, res);
+            break;
+        case "PATCH":
+            await userController.updateUser(req, res);
+            break;
+    
+        default:
+            break;
+    }
+    await userController.getUserById(req, res);
 }
