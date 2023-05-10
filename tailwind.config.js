44373d3280abb1d71429193d06ac98993752b7a2
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-10 10:34:47 +0200

    added brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors:{
+      'background': "#121218",
+      'foreground': '#141921',
+      'cardColor': "#2C333D",
+      'primary': '#061B3B',
+      'online': '#47F258'
+    }
   },
   plugins: [],
 }
