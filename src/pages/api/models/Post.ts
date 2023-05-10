42523477a1b5a02eed697b348b125c24e59ac9a0
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-10 10:50:02 +0200

    fix: resolve types errors in post class

--- src/pages/api/models/Post.ts
+++ src/pages/api/models/Post.ts
@@ -1,64 +1,60 @@
-import { PrismaClient } from "@prisma/client";
-import { Like } from "./Like";
-import { Tags } from "./Tags";
+// import { Tag } from "@prisma/client";
+import { Comment } from "./Comment";
 import { User } from "./User";
 export class Post {
   private readonly id: string;
   private title: string;
   private content: string;
-  private imagePost?: string | undefined;
-  private createdAt?: Date;
+  // private imagePost?: string | undefined;
+  private readonly createdAt: Date;
   private updatedAt?: Date;
-  private likes?: Like[] | undefined;
-  private comments?: Comment[] | undefined;
-  private tags?: Tags[] | undefined ;
-  private author: Partial<User>;
+  private author: {id: string, name: string} | User;
+  private tags: string[] | undefined ;
+  private readonly likes?: string[];
+  private readonly comments?: Partial<Comment[]> | undefined;
 
-  constructor(id: string, title: string, content: string, createdAt: Date, updatedAt: Date, author: Partial<User>, likes: Like[] = [], comments: Comment[] = [], tags: Tags[] = []) {
+  constructor(id: string, title: string, content: string, createdAt: Date, updatedAt: Date, author: {id: string, name: string}, tags: string[] = [], likes: string[] = [], comments: Comment[] = []) {
     this.id = id;
     this.title = title;
     this.content = content;
     this.createdAt = createdAt
     this.updatedAt = updatedAt
     this.author = author;
+    this.tags = tags;
     this.likes = likes;
     this.comments = comments;
-    this.tags = tags;
   }
 
-  public getId(): string{
-    return this.id
-  }
+  // public getId(): string{
+  //   return this.id
+  // }
 
-  public getTitle(): string{
-    return this.title;
-  }
+  // public getTitle(): string{
+  //   return this.title;
+  // }
   
-  public getContent(): string{
-    return this.content;
-  }
+  // public getContent(): string{
+  //   return this.content;
+  // }
 
-  public getCreatedAt(): Date | undefined{
-    return this.createdAt;
-  }
+  // public getCreatedAt(): Date | undefined{
+  //   return this.createdAt;
+  // }
 
-  public getUpdatedAt(): Date | undefined{
-    return this.updatedAt;
-  }
+  // public getUpdatedAt(): Date | undefined{
+  //   return this.updatedAt;
+  // }
 
-  public getLikes(): Like[] | undefined{
-    return this.likes;
-  }
+  // public getLikes(): string[] | undefined{
+  //   return this.likes;
+  // }
 
-  public getComments(): Comment[] | undefined{
-    return this.comments;
-  }
-  public getTags(): Tags[] | undefined{
-    return this.tags;
-  }
-  public getAuthor(): Partial<User>{
-    return this.author;
-  }
+  // public getComments(): Comment[] | undefined{
+  //   return this.comments;
+  // }
+  // public getTags(): string[] | undefined{
+  //   return this.tags;
+  // }
 
   
 
