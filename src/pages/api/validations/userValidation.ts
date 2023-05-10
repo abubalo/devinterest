7925c90f8a75c1fc9373f09be0412eb7dd14d126
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-10 12:42:44 +0200

    fix: resolve return validation error

--- src/pages/api/validations/userValidation.ts
+++ src/pages/api/validations/userValidation.ts
@@ -4,15 +4,11 @@ import { User } from "../models/User";
 export const validateCreateUser = async (data: Partial<User>) => {
   const schema = Joi.object({
     name: Joi.string().min(3).max(30).required(),
-    email: Joi.string().required(),
-    bio: Joi.string(),
-    gender: Joi.string(),
+    email: Joi.string().email().required(),
     password: Joi.string()
       .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
       .required(),
-    avatarUrl: Joi.string().required(),
-    location: Joi.string().required(),
   });
 
-  return schema;
+  return schema.validateAsync(data);
 };
