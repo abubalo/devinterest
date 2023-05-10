102f6be573427704d963ce3ca5957a3895447957
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-10 10:53:17 +0200

    feat: implent get post by id endpoint

--- src/pages/api/controllers/postController.ts
+++ src/pages/api/controllers/postController.ts
@@ -7,14 +7,14 @@ class PostController {
 
   public async createPost(req: NextApiRequest, res: NextApiResponse): Promise<void> {
       try {
-        const { id, title, content, tags } = req.body;
-        const post = await postService.createPost( id, title, content, tags);
+        const { title, content, authorId, tags } = req.body;
+        const post = await postService.createPost( title, content, authorId, tags);
         res.status(200).json(post);
     } catch (error: any) {
       console.log(error.message);
       res.status(500).json({ error: "Internal server error!" });
     }
-  }
+  } 
 
   public async getAllPosts(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     try {
@@ -29,6 +29,28 @@ class PostController {
     }
   }
 
+  public async getPostsbyId(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    try {
+        const id = typeof req.query.id
+        
+        if(id == undefined){
+          res.status(400).json({ message: 'Invalid request or missing ID parameter' });
+          return;
+        }
+        const posts = await postService.getPostsbyId(id);
+
+        if(!posts){
+            res.status(404).json({error: "User does not have posts!"});
+            return;
+        }
+
+        res.status(200).json(posts);
+
+    } catch (error: any) {
+      console.log(error.message);
+      res.status(500).json({ error: "Unable to fetch posts"});
+    }
+  }
   public async getPostsbyAuthorId(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     try {
         const {id} = req.body;
@@ -77,6 +99,24 @@ class PostController {
       console.log(error.message);
       res.status(500).json({ error: "Unable to delete post!"});
     }
+  }
+
+  public async createComment(req: NextApiRequest, res:NextApiResponse): Promise<void>{
+    const {content, postId, authorId} = req.body;
+    try {
+      const comment = await postService.createComment(content, postId, authorId)
+
+    if(!comment){
+      res.status(404).json({error: "User not or post not found"})
+    }
+    res.status(200).json(comment);
+
+    } catch (error: any) {
+      console.log(error.message);
+      res.status(500).json("Unable to create comment")
+    }
+
+    
   }
 }
 
