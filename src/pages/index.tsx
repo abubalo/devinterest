88e15ba4f43c3402198995ede0ddc74eecb48be6
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-12 20:35:22 +0200

    add components(header, side, and feed) to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Siderbar from "@/components/navigation/Siderbar";
+import Header from "@/components/navigation/Header";
+import Feed from "@/components/content/Feed";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      
+    <main className="relative flex flex-col min-h-screen  bg-background">
+      <Header />
+      <div className="h-full flex">
+        <Siderbar />
+        <Feed />
+      </div>
     </main>
-  )
+  );
 }
