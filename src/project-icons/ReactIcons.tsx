01a534549663321b0266091bd892af5e8d43304b
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-12 20:36:38 +0200

    configure new icons

--- src/project-icons/ReactIcons.tsx
+++ src/project-icons/ReactIcons.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import {BsGoogle, BsGithub, BsImage, BsEmojiHeartEyes} from "react-icons/bs"
+import {MdGroup, MdGroupAdd} from "react-icons/md"
+import {SlOptionsVertical} from "react-icons/sl"
 
-const IconsConfig = () => {
-  return (
-    <div>IconsConfig</div>
-  )
-}
+export const GoogleIcon = () => <BsGoogle />
+export const GithubIcon = () => <BsGithub />
+export const FollowIcon = () => <MdGroupAdd />
+export const FollowersIcon = () => <MdGroup />
+export const MediaIcon = () => <BsImage />
+export const EmojiIcon = () => <BsEmojiHeartEyes />
+export const MoreOptionIcon = () => <SlOptionsVertical />
 
-export default IconsConfig
