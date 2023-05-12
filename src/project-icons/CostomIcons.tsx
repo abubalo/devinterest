ca00ecab0abe287214aa861a5eb19eb7135c30e1
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-12 20:55:37 +0200

    add svg icon

---
+++ src/project-icons/CostomIcons.tsx
@@ -0,0 +1,9 @@
+
+
+export const CostomIcons = () => <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="10.5" cy="10.5" r="9.5" stroke="#C8C8C9" stroke-width="2"/>
+<path d="M10.5 0.677414V19.6452" stroke="#C8C8C9" stroke-width="2"/>
+<path d="M1.01613 10.1613L19.9839 10.1613" stroke="#C8C8C9" stroke-width="2"/>
+</svg>
+
+export const dd = () => {}
