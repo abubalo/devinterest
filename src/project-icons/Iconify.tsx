dbb34015ce9e08788478f07cd8def38a65a7627a
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-12 20:56:27 +0200

    configure iconify icons

---
+++ src/project-icons/Iconify.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+export const Notification = () => <Icon icon="ion:notifcations" />;
+export const Message = () => <Icon icon="ic:baseline-message" />;
+export const Love = () => (
+  <Icon icon="streamline:interface-favorite-heart-reward-social-rating-media-heart-it-like-favorite-love" />
+);
+export const FeedIcon = () => <Icon icon="bxs:book-content" />;
+export const Comment = () => (
+  <Icon icon="material-symbols:mode-comment-outline" />
+);
+export const HamburgerMenu = () => <Icon icon="ci:hamburger-md" />;
+export const Explore = () => <Icon icon="ic:outline-explore" />;
+export const Premium = () => <Icon icon="fluent:premium-20-regular" />;
+export const EditIcon = () => <Icon icon="material-symbols:edit" />;
+export const GifIcon = () => <Icon icon="fluent:gif-16-regular" />;
+export const BookmarkIcon = () => <Icon icon="material-symbols:bookmark" />
+export const OptionsIcon = () => <Icon icon="mi:options-vertical" />
