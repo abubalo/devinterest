ca41ce7d61fc5bc91a1147d1a86ec558292f8cb4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-14 23:23:26 +0200

    Wrapped context provider around _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import {QueryClient, QueryClientProvider} from "react-query"
-import { ReactQueryDevtools} from "react-query/devtools"
+import UserContextProvider from "@/hooks/UserContext";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen/>
+      <UserContextProvider>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen />
+      </UserContextProvider>
     </QueryClientProvider>
-  )
+  );
 }
