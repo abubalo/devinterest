7dad9716077360389c5f72b6d7281d05e2127b66
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-15 22:14:23 +0200

    fix type mismatch in Props

--- src/components/post/Comments.tsx
+++ src/components/post/Comments.tsx
@@ -1,43 +1,50 @@
 import React from "react";
 import Image from "next/image";
+import {Comment} from "../../types/CommentTypes"
 
+type CommentProps = {
+  comments: Comment[] | null
+};
 
-const Comments = () => {
-  const replies =[
-    {name: "Mercy Alade"},
-    {name: "Abu Balo"}
-  ]
+const Comments = ({ comments }: CommentProps) => {
+
+  if(!comments){
+    return <p className="p-4 bg-neutral-700/50">No comment</p>
+  }
+  console.log("Comment:", comments)
+
   return (
     <>
-    <div className="ml-4 mt-4">
-      <div className="relative flex gap-4 items-start">
-        <div className=" w-8 h-8 overflow-hidden rounded-full aspect-square">
-          {/* online status */}
-          {/* <div className="absolute w-3 h-3 bg-online rounded-full bottom-3 left-[40px]"></div> */}
-          <Image
-            src="/assets/img1.jpg"
-            width={50}
-            height={50}
-            alt="user image"
-          />
-        </div>
-        <div>
-          <p className="text-sm">Janet Macron</p>
-          <p className="text-[0.6rem] text-gray-300 -">Dev Advocate</p>
-          <div>
-            <p className="text-[12px] text-gray-500">2 minutes ago</p>
-          </div>
-        </div>
-      </div>
+      {comments.map((comment) => (
+        <div key={comment.id}>
+          <div  className="ml-4 mt-4">
+            <div className="relative flex gap-4 items-start">
+              <div className=" w-8 h-8 overflow-hidden rounded-full aspect-square">
+                <Image
+                  src={comment?.author?.name ||"/assets/img1.jpg"}
+                  width={50}
+                  height={50}
+                  alt={comment?.author?.name || ""}
+                />
+              </div>
+              <div>
+                <p className="text-sm">{comment?.author?.name}</p>
+                <p className="text-[0.6rem] text-gray-300 -">{comment?.author?.name}</p>
+                <div>
+                  <p className="text-[12px] text-gray-500">{comment?.createdAt}</p>
+                </div>
+              </div>
+            </div>
 
-      <div className="text-sm font-light text-slate-50/80 text-justify">
-        <p className="leading-normal">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nam fuga
-          impedit officiis doloribus saepe.
-        </p>
-      </div>
-    </div>
-    {/* {replies && replies.map(reply =><>{<Comments />} </>)} */}
+            <div className="text-sm font-light text-slate-50/80 text-justify">
+              <p className="leading-normal">
+                {comment.content}
+              </p>
+            </div>
+          </div>
+          {comment.replies && comment.replies.map((reply: any) =><>{<Comments comments={reply} />} </>)}
+        </div>
+      ))}
     </>
   );
 };
