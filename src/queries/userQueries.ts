e3dcd3b2bcacb9c2e17701ae6fe29be3f6d703ff
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-15 22:18:56 +0200

    Refactor getUser function to handle errors and return null on failure

--- src/queries/userQueries.ts
+++ src/queries/userQueries.ts
@@ -1,40 +1,57 @@
 import { User } from "@prisma/client";
 import axios from "axios";
 
+export type UserData = Pick<User, "name" | "email" | "password">;
 
-export type UserData =  Pick<User, "name" | "email" | "password">
-
-export type LoginData = Pick<User, 'email' | 'password'>
+export type LoginData = Pick<User, "email" | "password">;
 
 const userApi = axios.create({
   baseURL: "http://localhost:3000/api/routes/user",
+  withCredentials: true,
 });
 
-
-export const getUser = async (): Promise<User> => {
-  const response = await userApi.get("/index");
-  return response.data;
+export const getUser = async (): Promise<User | null> => {
+  try {
+    const response = await userApi.get("");
+    return response.data; //Set withCredentials to true for all requests made with userApi
+  } catch (error: any) {
+    console.log(error.message);
+    return null
+  }
 };
 
-export const addUser = async (data: UserData ): Promise<User> => {
-  const response = await userApi.post("/create", data);
-  return response.data;
+export const addUser = async (data: UserData): Promise<User | null> => {
+  try {
+    const response = await userApi.post("/create", data);
+    return response.data;
+  } catch (error: any) {
+    console.log(error.message);
+    return null
+  }
 };
 
-export const loginUser = async (data: LoginData): Promise<User> => {
-  const response = await userApi.post("/login", data);
-  return response.data;
+export const loginUser = async (data: LoginData): Promise<User | null> => {
+  try {
+    const response = await userApi.post("/login", data);
+    return response.data;
+  } catch (error: any) {
+    console.log(error.message);
+    return null
+  }
 };
 
-export const updateUser = async (id: string, data: UserData): Promise<User> => {
-  const response = await userApi.patch(`/update/?id=${id}`, data);
-  return response.data;
+export const updateUser = async (id: string, data: UserData): Promise<User | null> => {
+  try {
+    const response = await userApi.patch(`/update/?id=${id}`, data);
+    return response.data;
+  } catch (error: any) {
+    console.log(error.message);
+    return null
+  }
 };
 
 export const deleteUser = async (id: string): Promise<void> => {
-  const response = await userApi.delete(`/delete/?id=${id}`);
-  return response.data
+  await userApi.delete(`/delete/?id=${id}`);
 };
 
-export default userApi
-
+export default userApi;
