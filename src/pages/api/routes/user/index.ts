38951688ef91ca306459f9d888ea7318e2ab53bb
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-17 23:25:47 +0200

    change user controller to uppercase

--- src/pages/api/routes/user/index.ts
+++ src/pages/api/routes/user/index.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import UserController from "../../controllers/userController";
 
-const userController = new UserController();
+
 
 // Get all users
 export default async function GET(req: NextApiRequest, res: NextApiResponse){
-    await userController.getUser(req, res);
+    await UserController.getUser(req, res);
 }
