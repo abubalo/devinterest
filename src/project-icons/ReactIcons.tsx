068696277cd714cde68a4d3dd81d804c978851b4
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-05-18 10:18:31 +0200

    add new Icons

--- src/project-icons/ReactIcons.tsx
+++ src/project-icons/ReactIcons.tsx
@@ -1,6 +1,7 @@
 import {BsGoogle, BsGithub, BsImage, BsEmojiHeartEyes} from "react-icons/bs"
-import {MdGroup, MdGroupAdd} from "react-icons/md"
+import {MdGroup, MdGroupAdd, MdOutlineArrowBackIosNew, MdOutlineCheck} from "react-icons/md"
 import {SlOptionsVertical} from "react-icons/sl"
+import {CgDanger} from "react-icons/cg"
 
 export const GoogleIcon = () => <BsGoogle />
 export const GithubIcon = () => <BsGithub />
@@ -9,4 +10,7 @@ export const FollowersIcon = () => <MdGroup />
 export const MediaIcon = () => <BsImage />
 export const EmojiIcon = () => <BsEmojiHeartEyes />
 export const MoreOptionIcon = () => <SlOptionsVertical />
+export const BackWard = () => <MdOutlineArrowBackIosNew />
+export const CheckMark = () => <MdOutlineCheck />
+export const DangerIcon = () => <CgDanger />
 
