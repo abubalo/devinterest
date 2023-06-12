022881e7bfceb1c13230c7da175d184b55297279
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-06-12 21:22:29 +0200

    fix props error in comment component

--- src/components/post/Comments.tsx
+++ src/components/post/Comments.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Image from "next/image";
-import {Comment} from "../../types/CommentTypes"
+import { Comment } from "../../types/CommentTypes";
 import PostedAgo from "./PostedAgo";
 
 type CommentProps = {
-  comments: Comment[] | undefined
+  comments: Comment[] | undefined;
 };
 
 const Comments = ({ comments }: CommentProps) => {
-  
 
-  if(!comments){
-    return <p className="p-4 bg-neutral-700/50">No comment...</p>
+  if(!comments || comments.length === 0){
+    return <div className="text-sm">No comment</div>
   }
-  console.log("Comment:", comments)
 
   return (
     <>
+   
       {comments?.map((comment) => (
         <div key={comment.id}>
-          <div  className="ml-4 mt-4">
+          <div className="ml-4 mt-4">
             <div className="relative flex gap-4 items-start">
               <div className=" w-8 h-8 overflow-hidden rounded-full aspect-square">
                 <Image
-                  src={comment?.author?.name ||"/assets/img1.jpg"}
+                  src={comment?.author?.name || "/assets/img1.jpg"}
                   width={50}
                   height={50}
                   alt={comment?.author?.name || ""}
@@ -31,21 +30,26 @@ const Comments = ({ comments }: CommentProps) => {
               </div>
               <div>
                 <p className="text-sm">{comment?.author?.name}</p>
-                <p className="text-[0.6rem] text-gray-300 -">{comment?.author?.name}</p>
+                <p className="text-[0.6rem] text-gray-300 -">
+                  {comment?.author?.name}
+                </p>
                 <div>
-                  <p className="text-[12px] text-gray-500">{comment?.createdAt.toLocaleDateString()}</p>
+                  <p className="text-[12px] text-gray-500">
+                    {comment?.createdAt.toLocaleDateString()}
+                  </p>
                   <PostedAgo createdAt={comment.createdAt} />
                 </div>
               </div>
             </div>
 
             <div className="text-sm font-light text-slate-50/80 text-justify">
-              <p className="leading-normal">
-                {comment.content}
-              </p>
+              <p className="leading-normal">{comment.content}</p>
             </div>
           </div>
-          {comment.replies && comment.replies.map((reply: any) =><>{<Comments key={comment.id} comments={reply} />} </>)}
+          {/* {comment.replies &&
+            comment.replies.map((reply: any) => (
+              <>{<Comments key={comment.id} comments={reply.replies} />} </>
+            ))} */}
         </div>
       ))}
     </>
--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -1,7 +1,7 @@
 import { Comment, Love } from "@/project-icons/Iconify";
 import { FollowIcon, MoreOptionIcon } from "@/project-icons/ReactIcons";
 import Image from "next/image";
-import { useContext, memo } from "react";
+import { useContext } from "react";
 import Comments from "./Comments";
 import { getAllPosts } from "../../queries/postQueries";
 import { useQuery } from "react-query";
@@ -10,7 +10,7 @@ import PostedAgo from "./PostedAgo";
 
 const PostContent = () => {
   const { user } = useContext(UserContext);
-  
+
   const { data: posts, isLoading, error } = useQuery(["posts"], getAllPosts);
 
   if (isLoading) {
@@ -25,9 +25,7 @@ const PostContent = () => {
     <>
       {posts?.map((post) => (
         <div key={post.id}>
-          <div
-            className="w-full flex flex-col bg-foreground h-auto p-4 space-y-5 border border-slate-400/10 rounded-md"
-          >
+          <div className="w-full flex flex-col bg-foreground h-auto p-4 space-y-5 border border-slate-400/10 rounded-md">
             {/* User profile */}
             <div className="relative flex justify-between gap-4 items-start">
               <div className="flex gap-2">
@@ -51,11 +49,13 @@ const PostContent = () => {
                       <FollowIcon />
                     </span>
                   </span>
-                  <p className="text-sm text-gray-300 -mt-2">{user?.username}</p>
+                  <p className="text-sm text-gray-300 -mt-2">
+                    {user?.username}
+                  </p>
                   <div>
                     <p className="text-[12px] text-gray-500">
                       {/* {post?.createdAt} */}
-                      <PostedAgo createdAt={post.createdAt}/>
+                      <PostedAgo createdAt={post.createdAt} />
                     </p>
                   </div>
                 </div>
@@ -112,7 +112,7 @@ const PostContent = () => {
                 </div>
               </div>
             </div>
-            <Comments comments={post.comments} />
+            <Comments comments={post.comments || []} />
           </div>
         </div>
       ))}
@@ -120,4 +120,4 @@ const PostContent = () => {
   );
 };
 
-export default memo(PostContent) ;
+export default PostContent;
