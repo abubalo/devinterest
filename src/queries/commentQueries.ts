c5f5e56e7ccbfe328ad3d621cefe9d101553a8d7
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-06-14 23:40:02 +0200

    Fix comment type anotation error

--- src/queries/commentQueries.ts
+++ src/queries/commentQueries.ts
@@ -1,21 +1,23 @@
 import axios from "axios"
 import { Comment } from "@prisma/client";
 
+export type AddComment = Pick<Comment, "postId" | "authorId" | "content">;
+
 const commentApi = axios.create({
     baseURL: "http://localhost:3000/api/routes/comment"
 })
 
-// export const getComments = async (): Promise<Comment[]> =>{
-//     const response = await commentApi.get('/index');
-//     return response.data;
-// }
+export const getComments = async (): Promise<Comment[]> =>{
+    const response = await commentApi.get('/index');
+    return response.data;
+}
 
 export const getCommentsByPostId = async (postId: string): Promise<Comment> =>{
     const response = await commentApi.get(`/index/?id=${postId}`,);
     return response.data;
 }
 
-export const addComment = async (data: Comment): Promise<Comment> =>{
+export const addComment = async (data: AddComment): Promise<Comment> =>{
     const response = await commentApi.post('/create', data);
     return response.data;
 }
